85f4c2445f226459e534d65fe01ea7d0d2cb1501
Luna <basikallyluna@gmail.com>
2024-11-17 14:52:21 +0200

    Add remindme command

--- cogs/remindme.py
+++ cogs/remindme.py
@@ -1,9 +1,16 @@
+from funcs import actions
 from discord.ext import commands
 
 
 class Remindme(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.client.conn = client.conn
+
+    @commands.command()
+    async def remindme(self, ctx, time, *, message: str):
+        remind_at = actions.get_timedelta(time)
+        actions.add_reminder(self.client.conn, ctx.guild.id, ctx.author.id, remind_at, message)
 
 
 async def setup(client):
