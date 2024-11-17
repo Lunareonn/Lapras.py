e4ee550f87d499217a9e86d5d20cfb16393c1ea3
Luna <basikallyluna@gmail.com>
2024-11-17 13:55:42 +0200

    Create remindme cog

---
+++ cogs/remindme.py
@@ -0,0 +1,10 @@
+from discord.ext import commands
+
+
+class Remindme(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+
+async def setup(client):
+    await client.add_cog(Remindme(client))
