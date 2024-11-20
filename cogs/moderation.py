88cd24f31533c9b7dfea0bd0b85cf3daad788c55
Lunareonn <basikallyluna@gmail.com>
2024-11-20 12:45:55 +0200

    Rewrite kick command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -54,6 +54,46 @@ class Moderation(commands.Cog):
         actionlog_embed.add_field(name="Issuer:", value=ctx.author)
         await actionlogs_channel.send(embed=actionlog_embed)
 
+    @commands.command()
+    async def kick(self, ctx, user: Optional[discord.User], *, reason: str = ""):
+        if user is None and ctx.message.reference is None:
+            return await ctx.send("No user was specified!")
+        else:
+            if ctx.message.reference:
+                message = await ctx.channel.fetch_message(ctx.message.reference.message_id)
+                user = message.author
+
+        if user == ctx.author:
+            return await ctx.send("You can't kick yourself!")
+        if user == self.client.user:
+            return await ctx.send("I can't kick myself!")
+
+        direct_message = f"You have been kicked from {ctx.guild}"
+        if reason:
+            direct_message += f"Reason: {reason}"
+        direct_message += "\nYou can rejoin, but please behave this time."
+
+        await ctx.guild.kick(user, reason=reason)
+        await ctx.send(f"{user} has been banned! :thumbsup:")
+        actions.add_mod_record(self.client.conn, ctx.guild.id, ctx.author.id, user.id, reason, None, "kick")
+
+        try:
+            await user.send(direct_message)
+        except discord.HTTPException:
+            pass
+
+        try:
+            actionlogs_channel = await self.client.fetch_channel(actions.fetch_actionlog_channel(self.client.conn, ctx.guild.id))
+        except TypeError:
+            return
+
+        actionlog_embed = discord.Embed(title="User was kicked!", description=f"{user} has been kicked.")
+        actionlog_embed.set_author(name=f"{user.display_name} ({user})", icon_url=user.avatar.url)
+        if reason:
+            actionlog_embed.add_field(name="Reason:", value=reason, inline=True)
+        actionlog_embed.add_field(name="Issuer:", value=ctx.author)
+        await actionlogs_channel.send(embed=actionlog_embed)
+
 
 async def setup(client):
     await client.add_cog(Moderation(client))
